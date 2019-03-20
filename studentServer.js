7a138f258bf9216a133bdeb50bfa4fb160a58467
Chao-Hsien Cheng <ucfscde@ucl.ac.uk>
2019-03-20 10:36:28 -0400

    added veiw data geoJson

--- studentServer.js
+++ studentServer.js
@@ -67,6 +67,43 @@ app.post('/reflectData',function(req,res){
   res.send(req.body);
 });
 
+
+// Get From Data
+
+
+
+app.get('/getFormData/:port_id', function (req,res) {
+pool.connect(function(err,client,done) {
+if(err){
+console.log("not able to get connection "+ err);
+res.status(400).send(err);
+}
+// use the inbuilt geoJSON functionality
+// and create the required geoJSON format using a query adapted from here:
+
+// http://www.postgresonline.com/journal/archives/267-Creating-GeoJSON-Feature-Collections-with-JSON-and-PostGIS-functions.html, accessed 4th January 2018
+
+// note that query needs to be a single string with no line breaks so built it up bit by bit
+var querystring = " SELECT 'FeatureCollection' As type,array_to_json(array_agg(f)) As features FROM ";
+querystring = querystring + "(SELECT 'Feature' As type ,ST_AsGeoJSON(lg.geom)::json As geometry, ";
+querystring = querystring + "row_to_json((SELECT l FROM (SELECT name,surname, port_id) As l ";
+querystring = querystring + " )) As properties";
+querystring = querystring + " FROM formdata As lg where lg.port_id= '"+req.params.port_id + "' limit 100 ) As f ";
+console.log(querystring);
+client.query(querystring,function(err,result){
+//call `done()` to release the client back to the pool
+done();
+if(err){
+console.log(err);
+res.status(400).send(err);
+}
+res.status(200).send(result.rows);
+});
+});
+});
+
+
+
 // upload data
 
 app.post('/uploadData',function(req,res){
@@ -85,8 +122,7 @@ var portnum = req.body.port_id;
 var language = req.body.language;
 var modulelist = req.body.modulelist;
 var lecturetime = req.body.lecturetime;
-var geometrystring = "st_geomfromtext('POINT("+req.body.longitude + " "+
-req.body.latitude + ")')";
+var geometrystring = "st_geomfromtext('POINT("+req.body.longitude + " "+req.body.latitude + ")')";
 var querystring = "INSERT into formdata (name,surname,module, port_id,language,modulelist, lecturetime, geom) values ($1,$2,$3,$4,$5,$6,$7,";
 var querystring = querystring + geometrystring + ")";
  console.log(querystring);
