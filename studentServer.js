432571fe49f69152fd460eff416acf3026db2feb
Chao-Hsien Cheng <ucfscde@ucl.ac.uk>
2019-03-20 03:30:03 -0400

    Update studentServer.js

--- studentServer.js
+++ studentServer.js
@@ -2,13 +2,22 @@
 var express = require('express');
 var path = require("path");
 var app = express();
-2 of 6
+
 // add an http server to serve files to the Edge browser
 // due to certificate issues it rejects the https files if they are not
 // directly called in a typed URL
 var http = require('http');
 var httpServer = http.createServer(app);
 httpServer.listen(4480);
+
+// adding functionality to log the requests
+app.use(function (req, res, next) {
+var filename = path.basename(req.url);
+var extension = path.extname(filename);
+console.log("The file " + filename + " was requested.");
+next();
+});
+
 app.get('/',function (req,res) {
 res.send("hello world from the HTTP server");
 });
