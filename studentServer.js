19e9cf92f2ee17c688f9afd8e73ef8d732935044
Chao-Hsien Cheng <ucfscde@ucl.ac.uk>
2019-03-20 09:02:18 -0400

    Database Connected and routes tested

--- studentServer.js
+++ studentServer.js
@@ -1,6 +1,8 @@
 // express is the server that forms part of the nodejs program
 var express = require('express');
 var path = require("path");
+var fs = require('fs');
+var pg = require('pg'); 
 var app = express();
 
 // add an http server to serve files to the Edge browser
@@ -9,6 +11,50 @@ var app = express();
 var http = require('http');
 var httpServer = http.createServer(app);
 httpServer.listen(4480);
+
+
+// adding body parser
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({
+ extended: true
+}));
+
+
+app.use(bodyParser.json()); 
+
+// Db connection
+var configtext =
+""+fs.readFileSync("/home/studentuser/certs/postGISConnection.js");
+// now convert the configruation file into the correct format -i.e. a name/value pair array
+var configarray = configtext.split(",");
+var config = {};
+for (var i = 0; i < configarray.length; i++) {
+ var split = configarray[i].split(':');
+ config[split[0].trim()] = split[1].trim();
+}
+var pool = new pg.Pool(config); 
+
+
+
+// adding simple 
+
+app.get('/postgistest', function (req,res) {
+pool.connect(function(err,client,done) {
+ if(err){
+ console.log("not able to get connection "+ err); 
+ res.status(400).send(err);
+ }
+ client.query('SELECT name FROM london_poi' ,function(err,result) {
+ done();
+ if(err){
+ console.log(err);
+ res.status(400).send(err);
+ }
+ res.status(200).send(result.rows);
+ });
+ });
+}); 
+
 
 // adding functionality to log the requests
 app.use(function (req, res, next) {
