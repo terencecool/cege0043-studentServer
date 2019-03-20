f8f5ce5542b054ce5fc50808f88331c75126ecc5
Chao-Hsien Cheng <ucfscde@ucl.ac.uk>
2019-03-20 09:40:22 -0400

    Posted data added to Database

--- studentServer.js
+++ studentServer.js
@@ -56,6 +56,48 @@ pool.connect(function(err,client,done) {
 }); 
 
 
+// adding POST req
+
+app.post('/reflectData',function(req,res){
+  // note that we are using POST here as we are uploading data
+  // so the parameters form part of the BODY of the request rather than the RESTful API
+  console.dir(req.body);
+
+  // for now, just echo the request back to the client
+  res.send(req.body);
+});
+
+// upload data
+
+app.post('/uploadData',function(req,res){
+ // note that we are using POST here as we are uploading data
+ // so the parameters form part of the BODY of the request rather than theRESTful API
+ console.dir(req.body);
+ pool.connect(function(err,client,done) {
+ if(err){
+ console.log("not able to get connection "+ err);
+ res.status(400).send(err);
+ }
+var name = req.body.name;
+var surname = req.body.surname;
+var module = req.body.module;
+var portnum = req.body.port_id;
+ var querystring = "INSERT into formdata (name,surname,module, port_id) values ($1,$2,$3,$4) ";
+ console.log(querystring);
+ client.query( querystring,[name,surname,module,
+portnum],function(err,result) {
+ done();
+ if(err){
+ console.log(err);
+ res.status(400).send(err);
+ }
+ res.status(200).send("row inserted");
+ });
+ });
+}); 
+
+
+
 // adding functionality to log the requests
 app.use(function (req, res, next) {
 var filename = path.basename(req.url);
